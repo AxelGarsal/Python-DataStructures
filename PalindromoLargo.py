94234aeec598409f257f8fae736e2e81a9ed9728
AxelGarsal <313230015@pcpuma.acatlan.unam.mx>
2021-02-06 23:36:03 -0600

    cambie una linea para entenderlo mejor

---
+++ PalindromoLargo.py
@@ -0,0 +1,10 @@
+numero = input('dame un numero: ')
+print(numero[::-1])
+'''numero1 = 100
+numero2 = 100
+producto = numero1 * numero2
+if producto != producto[::-1]:
+    numero1 += 1
+    numero2 += 1
+else:
+    print(producto)'''
