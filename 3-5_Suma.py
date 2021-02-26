a31d504b76bbee7f4e7b448e3632827bdf7eff8c
AxelGarsal <313230015@pcpuma.acatlan.unam.mx>
2021-02-26 10:16:14 -0600

    agregue un codigo de la suma de los multiplos de 2 0 3

--- 3-5_Suma.py
+++ 3-5_Suma.py
@@ -6,5 +6,11 @@ suma = 0
 for i in range(1, 1000):
     if i % 3 == 0 or i % 5 == 0:
         suma += i
-        print(i, suma)
+        print(i,suma)
 print(suma)
+
+mult = 0
+for i in range(1, 1001):
+    if i % 2 == 0 or i % 3 == 0:
+        mult = mult +1
+print(mult)
